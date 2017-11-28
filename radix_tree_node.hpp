a880dbe72541aab91096c1f8d9cb12434b84d82a
fredoule2k <frederic@branger.be>
2017-11-28 11:20:30 +0100

    Added generic comparison predicate function to the template (by default std::less<K>). The nodes map will be sorted according to this

--- radix_tree_node.hpp
+++ radix_tree_node.hpp
@@ -2,14 +2,15 @@
 #define RADIX_TREE_NODE_HPP
 
 #include <map>
+#include <functional>
 
-template <typename K, typename T>
+template <typename K, typename T, typename Compare>
 class radix_tree_node {
-    friend class radix_tree<K, T>;
-    friend class radix_tree_it<K, T>;
+    friend class radix_tree<K, T, Compare>;
+    friend class radix_tree_it<K, T, Compare>;
 
     typedef std::pair<const K, T> value_type;
-    typedef typename std::map<K, radix_tree_node<K, T>* >::iterator it_child;
+    typedef typename std::map<K, radix_tree_node<K, T, Compare>*, Compare >::iterator it_child;
 
 private:
     radix_tree_node() : m_children(), m_parent(NULL), m_value(NULL), m_depth(0), m_is_leaf(false), m_key() { }
@@ -19,16 +20,16 @@ private:
 
     ~radix_tree_node();
 
-    std::map<K, radix_tree_node<K, T>*> m_children;
-    radix_tree_node<K, T> *m_parent;
+    std::map<K, radix_tree_node<K, T, Compare>*, Compare> m_children;
+    radix_tree_node<K, T, Compare> *m_parent;
     value_type *m_value;
     int m_depth;
     bool m_is_leaf;
     K m_key;
 };
 
-template <typename K, typename T>
-radix_tree_node<K, T>::radix_tree_node(const value_type &val) :
+template <typename K, typename T, typename Compare>
+radix_tree_node<K, T, Compare>::radix_tree_node(const value_type &val) :
     m_children(),
     m_parent(NULL),
     m_value(NULL),
@@ -39,8 +40,8 @@ radix_tree_node<K, T>::radix_tree_node(const value_type &val) :
     m_value = new value_type(val);
 }
 
-template <typename K, typename T>
-radix_tree_node<K, T>::~radix_tree_node()
+template <typename K, typename T, typename Compare>
+radix_tree_node<K, T, Compare>::~radix_tree_node()
 {
     it_child it;
     for (it = m_children.begin(); it != m_children.end(); ++it) {
