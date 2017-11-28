5913b4f1f73956f7ba299824c28fd95b0c3512a9
fredoule2k <frederic@branger.be>
2017-11-28 17:04:51 +0100

    template style fix

--- radix_tree_it.hpp
+++ radix_tree_it.hpp
@@ -5,8 +5,8 @@
 #include <functional>
 
 // forward declaration
-template <typename K, typename T, class Compare = std::less<K>> class radix_tree;
-template <typename K, typename T, class Compare = std::less<K>> class radix_tree_node;
+template <typename K, typename T, class Compare = std::less<K> > class radix_tree;
+template <typename K, typename T, class Compare = std::less<K> > class radix_tree_node;
 
 template <typename K, typename T, class Compare = std::less<K>>
 class radix_tree_it : public std::iterator<std::forward_iterator_tag, std::pair<K, T> > {
